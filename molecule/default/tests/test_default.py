4b46bbbc4251b49272f00914d39aa5fc5a6f416e
jasonwalsh <rightlag@gmail.com>
2020-04-07 15:18:23 -0400

    fix: use ec2 driver instead of docker driver
    Docker does not allow mutating the /etc/resolv.conf file, so the
    Molecule test life cycle must run in an EC2 instance.

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -3,9 +3,10 @@ import os
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']
-).get_hosts('all')
+    os.environ["MOLECULE_INVENTORY_FILE"]
+).get_hosts("all")
 
 
-def test_dns_socket(host):
-    assert host.socket("tcp://127.0.0.1:53").is_listening
+def test_dnsmasq_service(host):
+    assert host.service("dnsmasq").is_running
+    assert host.service("dnsmasq").is_enabled
